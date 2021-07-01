2f6af74a8cf763e46a958de66150c0161e8a148d
WissemAouichi <wissem.aouichi@gmail.com>
2021-07-01 17:06:06 +0100

    Formatted files

--- src/app/statistics/occupancy/components/price-variation/datepicker/datepicker.component.ts
+++ src/app/statistics/occupancy/components/price-variation/datepicker/datepicker.component.ts
@@ -1,53 +1,56 @@
-import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
-import { SohoDatePickerComponent } from 'ids-enterprise-ng';
+import { Component, HostListener, OnInit, ViewChild } from "@angular/core";
+import { SohoDatePickerComponent } from "ids-enterprise-ng";
 
 @Component({
-  selector: 'date-picker',
-  templateUrl: 'datepicker.component.html'
+  selector: "date-picker",
+  templateUrl: "datepicker.component.html",
 })
 export class DatePickerComponent {
   public innerWidth: any;
   public displayDatepicker: boolean;
-  public dateRange
+  public dateRange;
 
-  @ViewChild(SohoDatePickerComponent, { static: true }) datepicker?: SohoDatePickerComponent;
+  @ViewChild(SohoDatePickerComponent, { static: true })
+  datepicker?: SohoDatePickerComponent;
   dateFormat = Soho.Locale.currentLocale.data.calendars[0].dateFormat.short;
-  
 
   public model = {
-    standard: '2020/01/01',
-    validrange: '2020/01/01',
-    range: '12/12/2016 - 12/26/2016',
-    range2: '1/12/2017 - 1/16/2017',
+    standard: "2020/01/01",
+    validrange: "2020/01/01",
+    range: "12/12/2016 - 12/26/2016",
+    range2: "1/12/2017 - 1/16/2017",
   };
   public showModel = false;
   public datepickerDisabled = false;
   public datepickerReadOnly = false;
 
   public disableOptions: SohoDatePickerDisable = {
-    minDate: '2020/01/01',
-    maxDate: '2020/01/31',
+    minDate: "2020/01/01",
+    maxDate: "2020/01/31",
   };
 
   public datePickerOptions: SohoDatePickerOptions = {
     showTime: true,
-    timeFormat: 'HH:mm:ss',
+    timeFormat: "HH:mm:ss",
     minuteInterval: 10,
     secondInterval: 10,
     roundToInterval: true,
-    dateFormat: 'yyyy/dd/mm',
-    placeholder: 'placeholder',
+    dateFormat: "yyyy/dd/mm",
+    placeholder: "placeholder",
     showLegend: true,
     showMonthYearPicker: true,
-    legend: [{ name: 'Weekends', color: '#EFA836', dayOfWeek: [0, 6] }],
-    calendarName: 'gregorian'
+    legend: [{ name: "Weekends", color: "#EFA836", dayOfWeek: [0, 6] }],
+    calendarName: "gregorian",
   };
 
-  constructor() { }
+  constructor() {}
 
   onChange(event: SohoDatePickerEvent) {
-    console.log('DatePickerDemoComponent.onChange: type=' + event.data, 'Has originalEvent: ' + (event.originalEvent !== undefined));
-    console.log(this.model.validrange)
+    console.log(
+      "DatePickerDemoComponent.onChange: type=" + event.data,
+      "Has originalEvent: " + (event.originalEvent !== undefined)
+    );
+    console.log(this.model.validrange);
   }
 
   /* 
